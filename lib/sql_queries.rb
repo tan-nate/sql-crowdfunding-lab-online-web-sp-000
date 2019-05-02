336327f9a2eead5ce15c6c715a7348b7f92aa8c8
Nate Tan <nktan93@gmail.com>
2019-05-02 18:23:49 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -51,5 +51,10 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "SELECT Projects.category, "
+  "SELECT Projects.category, SUM(Pledges.amount)
+  FROM Pledges
+  INNER JOIN Projects
+  ON Pledges.project_id = Projects.id
+  GROUP BY Projects.category
+  WHERE Projects.category = 'music;"
 end
